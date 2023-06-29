cfecf2d7ec811cfd980c257b2c9c3632fce228dd
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-06-29 09:16:23 +0530

    next-auth_nav

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,8 +5,6 @@ const Footer = () => {
   return (
     <>
     <footer className="w-full bg-yellow-300 mt-2 flex justify-center items-center flex-col">
-        {/* <span className=''>Here you will find list of latest released Animes a perfect place for weebs</span> */}
-
        <div className="flex gap-4 h-8 justify-center items-center my-3">
            <span className='border-r-2 pr-1 border-r-slate-800'>Follow us</span>
            <Link href={process.env.NEXT_PUBLIC_GITHUB}><AiOutlineGithub className='w-6 h-6 cursor-pointer'/></Link>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,27 @@
+import { useSession ,signIn, signOut, } from 'next-auth/react';
+import Image from 'next/image';
 export const Nav=()=>{
+    const session=useSession();
+    const user=session?.data?.user;
+    console.log(user?.image)
     return(
-        <h3 className="w-full h-11 bg-yellow-300">navbar</h3>
+        <div className="flex justify-between items-center w-full h-12 bg-yellow-300 px-4">
+            <span><b><i>Weeb E Fied</i></b></span>
+            <input type="search" name="" id="" className='focus:outline-none w-1/2 border-b-slate-800 border-b-2 bg-transparent pl-2 text-slate-800' placeholder='Search'/>
+            {
+                session.status=='authenticated'?(
+                    <div className='w-10 h-10 relative rounded-2xl'>
+                        <Image
+                            src={user.image}
+                            width={100}
+                            height={100}
+                            alt='user Image'
+                            className='absolute  rounded-2xl cursor-pointer'
+                            onClick={()=>{void signOut()}}/>
+                    </div>
+                ):(<button type="" className='w-20 h-10 border-slate-800 border-2 p-1 rounded-lg hover:bg-slate-800 hover:text-yellow-300' onClick={()=>{void signIn()}}>Sign In</button>)
+            }
+        </div>
     )
 }
 export default Nav
